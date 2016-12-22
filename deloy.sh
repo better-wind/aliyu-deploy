dbd6bab2520b7fa55e3d672c4274dcababd401ed
fenghou <fenghou@ixiaopu.com>
2016-12-22 16:10:16 +0800

    deploy

---
+++ deloy.sh
@@ -0,0 +1,14 @@
+WEB_PATH='/admin/fenghou/www/aliyun-blog/'
+WEB_USER='lovelucydev'
+WEB_USERGROUP='lovelucydev'
+
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Finished."
---
+++ deploy.js
@@ -0,0 +1,25 @@
+var http = require('http')
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/gitdeploy', secret: 'fenghou' })
+var process = require('child_process');
+var dirPath ='/admin/fenghou/www/aliyun-blog/';
+
+http.createServer(function (req, res) {
+    handler(req, res, function (err) {
+        res.statusCode = 404
+        res.end('no such location')
+    })
+}).listen(7777)
+
+handler.on('error', function (err) {
+    console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+    process.exec('cd '+dirPath+' && git pull origin master', function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        console.log('========= pull master ok ==========');
+    });
+})
