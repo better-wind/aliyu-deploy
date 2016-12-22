4de90a44e90c168fb9e3756bccf1eb5943e1793e
fenghou <fenghou@ixiaopu.com>
2016-12-22 17:31:09 +0800

    deploy

--- deploy.js
+++ deploy.js
@@ -16,7 +16,7 @@ handler.on('error', function (err) {
 })
 
 handler.on('push', function (event) {
-    console.log('========= pull pull ==========');
+    console.log('========= pull master start ==========');
     process.exec('cd '+dirPath+' && git pull origin master', function (error, stdout, stderr) {
         if (error !== null) {
             console.log('exec error: ' + error);
