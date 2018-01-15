96667b3c27a5ac4ed492b8b0c1d78108ddb18f75
luyi10year <luyi_shinian@sina.cn>
2018-01-15 15:47:03 +0800

    modify

--- deploy.js
+++ deploy.js
@@ -1,8 +1,11 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/gitdeploy', secret: 'fenghou' })
+var handlerVi = createHandler({ path: '/gitdeployV', secret: 'vivi' })
+
 var process = require('child_process');
 var dirPath ='/admin/fenghou/www/aliyun-blog/';
+var dirPathVi ='/admin/fenghou/www/vv-blog/';
 
 http.createServer(function (req, res) {
     handler(req, res, function (err) {
@@ -18,6 +21,26 @@ handler.on('error', function (err) {
 handler.on('push', function (event) {
     console.log('========= pull master start ==========');
     process.exec('cd '+dirPath+' && git pull origin master', function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+        console.log('========= pull master ok ==========');
+    });
+})
+http.createServer(function (req, res) {
+    handlerVi(req, res, function (err) {
+        res.statusCode = 404
+        res.end('no such location')
+    })
+}).listen(7777)
+
+handlerVi.on('error', function (err) {
+    console.error('Error:', err.message)
+})
+
+handlerVi.on('push', function (event) {
+    console.log('========= pull master start ==========');
+    process.exec('cd '+dirPathVi+' && git pull origin master', function (error, stdout, stderr) {
         if (error !== null) {
             console.log('exec error: ' + error);
         }
